6ebc68fb54b883b7cf5c783d20cd28efb66f6204
Artem Baguinski <abaguinski@gmail.com>
2019-12-30 21:31:20 +0100

    Paste the vue-babylon demo from their readme

--- babylon-vue/src/App.vue
+++ babylon-vue/src/App.vue
@@ -1,8 +1,15 @@
 <template lang="pug">
-Scene
-  template(v-for="x in [0, 2, -2, 4, -4]")
-    template(v-for="y in [0, 2, -2, 4, -4]")
-      Box(v-for="z in [0, 2, -2, 4, -4]" :position="[x, y, z]" :key="`${x},${y},${z}`")
+  Scene
+    Camera
+    HemisphericLight(diffuse="#0000FF")
+    Entity(:position="[0, 0, 5]")
+      Animation(property="rotation.x" :duration="5" :end="Math.PI * 2")
+      Animation(property="rotation.y" :duration="5" :end="Math.PI * 2")
+      Animation(property="rotation.z" :duration="5" :end="Math.PI * 2")
+      PointLight(diffuse="#FF0000")
+      template(v-for="x in [0, 4, -4]")
+        template(v-for="y in [0, 4, -4]")
+          Box(v-for="z in [0, 4, -4]" :position="[x, y, z]" :key="`${x},${y},${z}`")
 </template>
 
 <script>
