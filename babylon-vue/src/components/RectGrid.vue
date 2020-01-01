cc7ac1055e386dbf3c0d1e776e68eedc2e71a007
Artem Baguinski <abaguinski@gmail.com>
2020-01-01 19:00:23 +0100

    Format the source

--- babylon-vue/src/components/RectGrid.vue
+++ babylon-vue/src/components/RectGrid.vue
@@ -3,7 +3,12 @@ div
   template(v-for="i1 in size[0]")
     template(v-for="j1 in size[1]")
       template(v-for="k1 in size[2]")
-        RectGridCell(:i="i1 - 1" :j="j1 - 1" :k="k1 - 1" :size="size" :spread="spread")
+        RectGridCell(
+          :i="i1 - 1" 
+          :j="j1 - 1" 
+          :k="k1 - 1" 
+          :size="size" 
+          :spread="spread")
           template(#default="cell")
             slot(:cell="cell")
 </template>
