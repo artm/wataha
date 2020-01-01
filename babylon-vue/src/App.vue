f328ce2fe15a21c6663be2c9c956ae07ba8e3cc8
Artem Baguinski <abaguinski@gmail.com>
2020-01-01 18:40:18 +0100

    Introduce another helper component for the grid cell

--- babylon-vue/src/App.vue
+++ babylon-vue/src/App.vue
@@ -6,10 +6,10 @@ Scene(fog="exp" fogColor="#000" :fogDensity=".15")
   Entity
     Animation(property="rotation.x" :duration="12" :end="Math.PI * 2")
     Animation(property="rotation.y" :duration="12" :end="Math.PI * 4")
-    RectGrid(:size="[5,5,5]")
-      template(#default="c")
-        Box(:position="[c.ii * 2, c.jj * 2, c.kk * 2]")
-          Material(:diffuse="[c.di, c.dj, c.dk]" :metallic=".3" :alpha=".5")
+    RectGrid(:size="[5,5,5]" :spread="1.5")
+      template(#default="grid")
+        Box
+          Material(:diffuse="grid.cell.dv" :metallic=".3" :alpha=".9")
 </template>
 
 <script>
@@ -19,4 +19,3 @@ export default {
   components: { RectGrid }
 }
 </script>
-
--- babylon-vue/src/components/RectGrid.vue
+++ babylon-vue/src/components/RectGrid.vue
@@ -3,16 +3,17 @@ div
   template(v-for="i1 in size[0]")
     template(v-for="j1 in size[1]")
       template(v-for="k1 in size[2]")
-        slot(
-          :i="i1 - 1" :j="j1 - 1" :k="k1 - 1" 
-          :ii="i1 - (size[0] + 1)/2" :jj="j1 - (size[1] + 1)/2" :kk="k1 - (size[2] + 1)/2"
-          :di="(i1 - 1)/size[0]" :dj="(j1 - 1)/size[1]" :dk="(k1 - 1)/size[2]"
-        )
+        RectGridCell(:i="i1 - 1" :j="j1 - 1" :k="k1 - 1" :size="size" :spread="spread")
+          template(#default="cell")
+            slot(:cell="cell")
 </template>
 
 <script>
+import RectGridCell from "./RectGridCell"
+
 export default {
   data: () => { return { } },
-  props: ["size"]
+  props: ["size", "spread"],
+  components: { RectGridCell }
 }
 </script>
