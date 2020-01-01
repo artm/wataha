34dae28a55feb78f4c452aa29eeea4aa59d87679
Artem Baguinski <abaguinski@gmail.com>
2020-01-01 20:49:40 +0100

    Play with more animations

--- babylon-vue/src/App.vue
+++ babylon-vue/src/App.vue
@@ -4,11 +4,17 @@ Scene(fog="exp" fogColor="#000" :fogDensity=".15")
   Camera(type="arcRotate")
   HemisphericLight
   Entity
-    Animation(property="rotation.x" :duration="120" :end="Math.PI * 2")
-    Animation(property="rotation.y" :duration="120" :end="Math.PI * 4")
-    RectGrid(:size="[5,5,5]" :spread="1.5")
+    Animation(property="rotation.x" :duration="60" :end="Math.PI * 2")
+    Animation(property="rotation.y" :duration="60" :end="Math.PI * 8")
+    RectGrid(:size="[5,5,5]" :spread="3")
       template(#default="grid")
-        Box
+        IcoSphere
+          Animation(property="rotation.z" :duration="10" :end="Math.PI * 2")
+          Animation(property="rotation.x" :duration="5" :end="Math.PI * 4")
+          Animation(property="material.alpha" :duration="5")
+            Key(frame="0%" :value="0.2")
+            Key(frame="50%" :value="0.95")
+            Key(frame="100%" :value="0.2")
           Material(:diffuse="grid.cell.dv" :metallic="0.75" :alpha=".5")
 </template>
 
