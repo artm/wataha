4e4b95d50c03cba7e478b63afb1853c599ceec6b
Artem Baguinski <abaguinski@gmail.com>
2020-01-02 11:08:15 +0100

    Extract Roto and CubeDemo components

--- babylon-vue/src/App.vue
+++ babylon-vue/src/App.vue
@@ -1,27 +1,11 @@
 <template lang="pug">
-Scene(fog="exp" fogColor="#000" :fogDensity=".15")
-  Property(name="clearColor" color="#000")
-  Camera(type="arcRotate")
-  HemisphericLight
-  Entity
-    Animation(property="rotation.x" :duration="60" :end="Math.PI * 2")
-    Animation(property="rotation.y" :duration="60" :end="Math.PI * 8")
-    RectGrid(:size="[5,5,5]" :spread="3")
-      template(#default="grid")
-        IcoSphere
-          Animation(property="rotation.z" :duration="10" :end="Math.PI * 2")
-          Animation(property="rotation.x" :duration="5" :end="Math.PI * 4")
-          Animation(property="material.alpha" :duration="5")
-            Key(frame="0%" :value="0.2")
-            Key(frame="50%" :value="0.95")
-            Key(frame="100%" :value="0.2")
-          Material(:diffuse="grid.cell.dv" :metallic="0.75" :alpha=".5")
+CubeDemo
 </template>
 
 <script>
-import RectGrid from "./components/RectGrid.vue"
+import CubeDemo from "./components/CubeDemo.vue"
 
 export default {
-  components: { RectGrid }
+  components: { CubeDemo }
 }
 </script>
---
+++ babylon-vue/src/components/Roto.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+Entity
+  Animation(v-if="x" property="rotation.x" :duration="x" :end="Math.PI *2")
+  Animation(v-if="y" property="rotation.y" :duration="y" :end="Math.PI *2")
+  Animation(v-if="z" property="rotation.z" :duration="z" :end="Math.PI *2")
+  slot
+</template>
+
+<script>
+export default {
+  data() { return {} },
+  props: ["x", "y", "z"]
+}
+</script>
