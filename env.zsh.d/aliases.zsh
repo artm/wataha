b86867ab67a1376ba71801a61293cc466e890a43
Artem Baguinski <abaguinski@gmail.com>
2020-01-03 09:48:14 +0100

    Add npx and webpack aliases

--- env.zsh.d/aliases.zsh
+++ env.zsh.d/aliases.zsh
@@ -1,4 +1,6 @@
 echo "Loading project aliases"
 alias dc="docker-compose"
 alias npm="dc run --rm node npm"
+alias npx="dc run --rm node npx"
 alias yarn="dc run --rm node yarn"
+alias webpack="npx webpack"
